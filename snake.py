45029ca3b68f1584a7ef9031e2b622172bd74730
Suyoscar11 <suyoscar11@gmail.com>
2024-09-24 00:06:59 -0600

    made the snake move horizontally in positive x direction

--- snake.py
+++ snake.py
@@ -1,1 +1,27 @@
 from turtle import Screen, Turtle
+import turtle
+import time
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+starting_positions = [(0,0) , (-20,0), (-40,0)]
+segments = []
+
+for positions in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(positions)
+    segments.append(new_segment)
+
+game_is_running = True
+while game_is_running:
+    screen.update()
+    time.sleep(0.1)
+    for seg in segments:
+        seg.forward(20)
+        
+turtle.done()
